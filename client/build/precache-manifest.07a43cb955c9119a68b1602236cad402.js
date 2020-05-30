418eb7807e062d559f7538f03b7254a403f54287
Ohad <ohadnave@gmail.com>
2020-05-31 01:51:41 +0300

    prodaction

---
+++ client/build/precache-manifest.07a43cb955c9119a68b1602236cad402.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bea78e7519765ee41c0951fcf315f5cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "16733a65f59d68098e80",
+    "url": "/static/js/2.a4771b25.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/static/js/2.a4771b25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c904b8844477adb88208",
+    "url": "/static/js/main.8bb447c1.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  }
+]);
